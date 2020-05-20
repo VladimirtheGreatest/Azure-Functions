09165a9ad6ffc6944ebc33d63d229d5960856e84
Vladimir <vladkodoktorov@gmail.com>
2020-05-20 22:25:28 +0100

    last test

--- GitHubMonitorApp/GitHubMonitor.cs
+++ GitHubMonitorApp/GitHubMonitor.cs
@@ -30,44 +30,22 @@ namespace GitHubMonitorApp
     }
 }
 
-//testing azure function again
+
 public class Rootobject
 {
-    public string zen { get; set; }
-    public int hook_id { get; set; }
-    public Hook hook { get; set; }
+    public string _ref { get; set; }
+    public string before { get; set; }
+    public string after { get; set; }
     public Repository repository { get; set; }
+    public Pusher pusher { get; set; }
     public Sender sender { get; set; }
-}
-
-public class Hook
-{
-    public string type { get; set; }
-    public int id { get; set; }
-    public string name { get; set; }
-    public bool active { get; set; }
-    public string[] events { get; set; }
-    public Config config { get; set; }
-    public DateTime updated_at { get; set; }
-    public DateTime created_at { get; set; }
-    public string url { get; set; }
-    public string test_url { get; set; }
-    public string ping_url { get; set; }
-    public Last_Response last_response { get; set; }
-}
-
-public class Config
-{
-    public string content_type { get; set; }
-    public string insecure_ssl { get; set; }
-    public string url { get; set; }
-}
-
-public class Last_Response
-{
-    public object code { get; set; }
-    public string status { get; set; }
-    public object message { get; set; }
+    public bool created { get; set; }
+    public bool deleted { get; set; }
+    public bool forced { get; set; }
+    public object base_ref { get; set; }
+    public string compare { get; set; }
+    public Commit[] commits { get; set; }
+    public Head_Commit head_commit { get; set; }
 }
 
 public class Repository
@@ -118,9 +96,9 @@ public class Repository
     public string labels_url { get; set; }
     public string releases_url { get; set; }
     public string deployments_url { get; set; }
-    public DateTime created_at { get; set; }
+    public int created_at { get; set; }
     public DateTime updated_at { get; set; }
-    public DateTime pushed_at { get; set; }
+    public int pushed_at { get; set; }
     public string git_url { get; set; }
     public string ssh_url { get; set; }
     public string clone_url { get; set; }
@@ -145,10 +123,14 @@ public class Repository
     public int open_issues { get; set; }
     public int watchers { get; set; }
     public string default_branch { get; set; }
+    public int stargazers { get; set; }
+    public string master_branch { get; set; }
 }
 
 public class Owner
 {
+    public string name { get; set; }
+    public string email { get; set; }
     public string login { get; set; }
     public int id { get; set; }
     public string node_id { get; set; }
@@ -167,6 +149,12 @@ public class Owner
     public string received_events_url { get; set; }
     public string type { get; set; }
     public bool site_admin { get; set; }
+}
+
+public class Pusher
+{
+    public string name { get; set; }
+    public string email { get; set; }
 }
 
 public class Sender
@@ -190,6 +178,66 @@ public class Sender
     public string type { get; set; }
     public bool site_admin { get; set; }
 }
+
+public class Head_Commit
+{
+    public string id { get; set; }
+    public string tree_id { get; set; }
+    public bool distinct { get; set; }
+    public string message { get; set; }
+    public DateTime timestamp { get; set; }
+    public string url { get; set; }
+    public Author author { get; set; }
+    public Committer committer { get; set; }
+    public object[] added { get; set; }
+    public object[] removed { get; set; }
+    public string[] modified { get; set; }
+}
+
+public class Author
+{
+    public string name { get; set; }
+    public string email { get; set; }
+    public string username { get; set; }
+}
+
+public class Committer
+{
+    public string name { get; set; }
+    public string email { get; set; }
+    public string username { get; set; }
+}
+
+public class Commit
+{
+    public string id { get; set; }
+    public string tree_id { get; set; }
+    public bool distinct { get; set; }
+    public string message { get; set; }
+    public DateTime timestamp { get; set; }
+    public string url { get; set; }
+    public Author1 author { get; set; }
+    public Committer1 committer { get; set; }
+    public object[] added { get; set; }
+    public object[] removed { get; set; }
+    public string[] modified { get; set; }
+}
+
+public class Author1
+{
+    public string name { get; set; }
+    public string email { get; set; }
+    public string username { get; set; }
+}
+
+public class Committer1
+{
+    public string name { get; set; }
+    public string email { get; set; }
+    public string username { get; set; }
+}
+
+
 
 
 
